050017dc52e5659d0c33b677772dcba092ba4e82
Javier Feliu <jfeliu@aesa.biz>
2019-05-05 13:37:20 -0500

    feat: Add lightbox gallery to graphics page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,45 +1,67 @@
-$(document).ready(function() {
-    var headerLogo$ = $(".header-logo");
-    var navbarMenu$ = $(".navbar-menu");
-    var navbarItem$ = $(".navbar-end a.navbar-item");
-    var navbarBurger$ = $(".navbar-burger");
+(function(app, $) {
+    $(document).ready(function() {
+        let headerLogo$ = $(".header-logo");
+        let navbarMenu$ = $(".navbar-menu");
+        let navbarItem$ = $(".navbar-end a.navbar-item");
+        let navbarBurger$ = $(".navbar-burger");
 
+        // Check for click events on the navbar burger icon
+        navbarBurger$.click(function() {
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            navbarBurger$.toggleClass("is-active");
+            navbarMenu$.toggleClass("is-active");
+            navbarItem$.toggleClass("is-active");
+            headerLogo$.toggleClass("is-active");
 
-    // Check for click events on the navbar burger icon
-    navbarBurger$.click(function() {
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        navbarBurger$.toggleClass("is-active");
-        navbarMenu$.toggleClass("is-active");
-        navbarItem$.toggleClass("is-active");
-        headerLogo$.toggleClass("is-active");
+            // Add menu animation
+            if (navbarMenu$.hasClass("is-active")) {
+                navbarMenu$.css({
+                    "animation-name": "menu-in",
+                    "animation-duration": ".75s"
+                });
+                headerLogo$.css({
+                    "animation-name": "hide-logo",
+                    "animation-duration": ".75s"
+                });
+                navbarItem$.css({
+                    "animation-name": "show-menu-item",
+                    "animation-duration": "1.5s"
+                });
+            } else {
+                navbarMenu$.css({
+                    "animation-name": "menu-out",
+                    "animation-duration": ".75s"
+                });
+                headerLogo$.css({
+                    "animation-name": "show-logo",
+                    "animation-duration": ".75s"
+                });
+                navbarItem$.css({
+                    "animation-name": "hide-menu-item",
+                    "animation-duration": ".75s"
+                });
+            }
+        });
 
-        // Add menu animation
-        if (navbarMenu$.hasClass("is-active")) {
-            navbarMenu$.css({
-                "animation-name": "menu-in",
-                "animation-duration": ".75s"
+        // Parses the website data.
+        $.getJSON('data.json', function(data) {
+            app.siteData = data;
+        });
+
+        app.openFB = function(index){
+            let imgs = app.siteData["galleries"]["graphics"][index - 1]["images"];
+            let fbItems = [];
+
+            // Builds a list of images to be displayed.
+            imgs.forEach(function(img) {
+                fbItems.push({
+                    src: "img/" + img,
+                    type: "image"
+                })
             });
-            headerLogo$.css({
-                "animation-name": "hide-logo",
-                "animation-duration": ".75s"
-            });
-            navbarItem$.css({
-                "animation-name": "show-menu-item",
-                "animation-duration": "1.5s"
-            });
-        } else {
-            navbarMenu$.css({
-                "animation-name": "menu-out",
-                "animation-duration": ".75s"
-            });
-            headerLogo$.css({
-                "animation-name": "show-logo",
-                "animation-duration": ".75s"
-            });
-            navbarItem$.css({
-                "animation-name": "hide-menu-item",
-                "animation-duration": ".75s"
-            });
-        }
+
+            console.log(fbItems);
+            $.fancybox.open(fbItems);
+        };
     });
-});
+})(window.app = window.app || {}, jQuery);
